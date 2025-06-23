530b6c00520ea15fefa9ab3c13bcc7faa776c109
klap <klap@klap-machine>
2025-06-23 21:25:18 +0200

    Improve benchmark plot.

--- benchmarks/on-voroif-gnn-dataset/benchmark.bash
+++ benchmarks/on-voroif-gnn-dataset/benchmark.bash
@@ -13,25 +13,20 @@ find ./input/structures/ -type f -name '*.pdb' \
 | shuf \
 | ../../voromarmotte --conda-path ~/miniconda3 -i _list --processors 32 --subselect-contacts '[-inter-chain]' --output-table-file "./output/global_scores_inter_chain.txt"
 
-[ -s "./output/global_scores_full.txt" ] || \
-find ./input/structures/ -type f -name '*.pdb' \
-| shuf \
-| ../../voromarmotte --conda-path ~/miniconda3 -i _list --processors 32 --subselect-contacts '[]' --output-table-file "./output/global_scores_full.txt"
-
 ################################################################################
 
 {
 cat << 'EOF'
-./output/global_scores_inter_chain.txt ./output/summary_of_selections_using_global_scores_inter_chain.txt
-./output/global_scores_full.txt ./output/summary_of_selections_using_global_scores_full.txt
+./output/global_scores_inter_chain.txt ./output/summary_of_selections_using_global_scores_inter_chain.txt ./output/plot_of_voromarmotte_global_scores_colored_by_cadscore_inter_chain.png
 EOF
 } \
-| while read -r INFILE OUTFILE
+| while read -r INFILE OUTFILE OUTFILEPLOT
 do
-R --vanilla --args "$INFILE" "$OUTFILE" << 'EOF' > /dev/null
+R --vanilla --args "$INFILE" "$OUTFILE" "$OUTFILEPLOT" << 'EOF' > /dev/null
 args=commandArgs(TRUE);
 infile=args[1];
 outfile=args[2];
+outfile_plot=args[3];
 
 df1=read.table(infile, header=TRUE, stringsAsFactors=FALSE);
 df1$area_persistence_win=(df1$area_expected_to_persist-df1$area_expected_to_vanish)/df1$area_total;
@@ -79,12 +74,22 @@ fraction_of_succesfull_selections=1-(num_of_failed_selections/num_of_targets);
 summary=data.frame(num_of_targets=num_of_targets, num_of_failed_selections=num_of_failed_selections, fraction_of_succesfull_selections=fraction_of_succesfull_selections);
 write.table(summary, file=outfile, row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t");
 
+df=df[order(df$area_total),];
 rgb_palette=colorRampPalette(c("red", "yellow", "green", "cyan", "blue"));
 thecolors=rgb_palette(100)
 color_indices=round(df$iface_cadscore*99)+1;
 assigned_colors=thecolors[color_indices];
-png(paste0(outfile, "_plot.png"));
-plot(df$area_expected_to_persist, df$area_expected_to_vanish, col=assigned_colors, cex=0.5, pch=19); abline(0, 1);
+png(outfile_plot, width=10, height=8, units="in", res=100);
+plot(
+	df$area_expected_to_persist, df$area_expected_to_vanish,
+	col=assigned_colors, cex=0.7, pch=1, log='xy', xlim=c(50, 3000), ylim=c(100, 3000),
+	xlab="Area expected to persist", ylab="Area expected to vanish",
+	main="VoroMarmotte global scores for native and predicted inter-chain interfaces,
+	colored by similarity to native interface (CAD-score)");
+abline(0, 1);
+legend("topleft", title="Interface CAD-score",
+	legend=c("[0, 0.2)", "[0.2, 0.4)", "[0.4, 0.6)", "[0.6, 0.8)", "[0.8, 1]"),
+	fill=c("red","yellow","green","cyan","blue"));
 dev.off()
 EOF
 
