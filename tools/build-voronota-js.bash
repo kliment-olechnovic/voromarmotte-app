ccd0f656ea7519023517d906d51d8d1e646dc522
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-09-07 16:23:57 +0300

    Enhanced binary build script.

--- tools/build-voronota-js.bash
+++ tools/build-voronota-js.bash
@@ -7,6 +7,9 @@ BUILDMODE="$1"
 if [ "$BUILDMODE" == "static" ]
 then
 	g++ -std=c++14 -static-libgcc -static-libstdc++ -static -O3 -I "./voronota-js-src/expansion_js/src/dependencies" -o "./voronota-js" $(find ./voronota-js-src/expansion_js/ -name '*.cpp')
+elif [ "$BUILDMODE" == "ofast" ]
+then
+	g++ -std=c++14 -O3 -march=native -I "./voronota-js-src/expansion_js/src/dependencies" -o "./voronota-js" $(find ./voronota-js-src/expansion_js/ -name '*.cpp')
 else
 	g++ -std=c++14 -O3 -I "./voronota-js-src/expansion_js/src/dependencies" -o "./voronota-js" $(find ./voronota-js-src/expansion_js/ -name '*.cpp')
 fi
