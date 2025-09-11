58fdc7bf8805995024ea5cea52ae9390fcad38a2
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-09-11 14:24:02 +0300

    Enhanced building script for the onnx-based tool.

--- tools/build-onnx-mlp-standalone.bash
+++ tools/build-onnx-mlp-standalone.bash
@@ -2,11 +2,59 @@
 
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-ORTDIR="$(pwd)/onnx-mlp-standalone-src/onnxruntime-linux-x64-1.19.2"
+DETECTED_OS=""
 
-g++ -O2 -std=c++14 \
-  ./onnx-mlp-standalone-src/onnx-mlp-standalone.cpp \
-  -I"${ORTDIR}/include" -L"${ORTDIR}/lib" \
-  -lonnxruntime -Wl,-rpath,'$ORIGIN/onnx-mlp-standalone-src/onnxruntime-linux-x64-1.19.2/lib' \
-  -o ./onnx-mlp-standalone
+if [[ "$OSTYPE" == darwin* ]]
+then
+	DETECTED_OS="macos"
+elif [[ "$OSTYPE" == linux* ]]
+then
+	DETECTED_OS="linux"
+fi
+
+if [ -z "$DETECTED_OS" ]
+then
+	UNAME_OSTYPE="$(uname -s)"
+	
+	if [[ "$UNAME_OSTYPE" == Darwin* ]]
+	then
+		DETECTED_OS="macos"
+	elif [[ "$UNAME_OSTYPE" == Linux* ]]
+	then
+		DETECTED_OS="linux"
+	fi
+fi
+
+if [ "$DETECTED_OS" != "linux" ]
+then
+	echo "Could not detect a supported OS for building the 'onnx-mlp-standalone' tool - it can only be built on Linux systems." 
+	exit 1
+fi
+
+if [ "$DETECTED_OS" != "linux" ] && [ "$DETECTED_OS" != "macos" ]
+then
+	echo "Could not detect a supported OS for building the 'onnx-mlp-standalone' tool - it can only be built on Linux or macOS systems." 
+	exit 1
+fi
+
+if [ "$DETECTED_OS" == "linux" ]
+then
+	ORTDIR="$(pwd)/onnx-mlp-standalone-src/onnxruntime-linux-x64-1.19.2"
+	
+	g++ -O2 -std=c++14 \
+	  ./onnx-mlp-standalone-src/onnx-mlp-standalone.cpp \
+	  -I"${ORTDIR}/include" -L"${ORTDIR}/lib" \
+	  -lonnxruntime -Wl,-rpath,'$ORIGIN/onnx-mlp-standalone-src/onnxruntime-linux-x64-1.19.2/lib' \
+	  -o ./onnx-mlp-standalone
+elif [ "$DETECTED_OS" == "macos" ]
+then
+	ORTDIR="$(pwd)/onnx-mlp-standalone-src/onnxruntime-osx-universal2-1.19.2"
+	
+	clang++ -O2 -std=c++14 \
+	  ./onnx-mlp-standalone-src/onnx-mlp-standalone.cpp \
+	  -I"${ORTDIR}/include" -L"${ORTDIR}/lib" \
+	  -lonnxruntime \
+	  -Wl,-rpath,@executable_path/onnx-mlp-standalone-src/onnxruntime-osx-universal2-1.19.2/lib \
+	  -o ./onnx-mlp-standalone
+fi
 
